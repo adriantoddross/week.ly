7b417573c634fa56b012777257b7810b3d73a0a9
Adrian Ross <adriantoddross@gmail.com>
2020-01-20 11:10:04 -0500

    implement head

---
+++ Linked List/linkedlist.js
@@ -0,0 +1,11 @@
+const Node = require('./node');
+
+class LinkedList {
+  /*
+    * The entry point into a Linked List is called the head.
+    * The head is a reference to the first node, or null (if the list is empty).
+  */
+  constructor() {
+    this.head = null;
+  }
+}
